88011a922c45aeb0d7f5bb5620de1772a90258aa
panyalei1987 <panyalei1987@163.com>
2021-09-10 04:19:24 +0800

    add  the vscode gdb debug

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pyl1987/github-source/arduino-A-2021
+# Build directory: /home/pyl1987/github-source/arduino-A-2021/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
